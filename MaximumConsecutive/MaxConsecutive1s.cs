9ecd893e891f9f412dae3e8a5569680918078e99
Ogiogio23 <emmanuel.ogiogio@gmail.com>
2021-04-09 16:43:12 +0100

    Add project files.

---
+++ MaximumConsecutive/MaxConsecutive1s.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumConsecutive
+{
+    class MaxConsecutive1s
+    {
+        public static int Maximum(int[] arr)
+        {
+            int counter = 0;
+            if (arr == null)
+            {
+                return 0;
+            }
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    if (arr[i] == arr[i + 1])
+                    {
+                        counter++;
+                    }
+                }
+
+            }
+            return counter;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0 };
+
+            Console.WriteLine(Maximum(arr));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SoccerBettingSite/SoccerBettingSite/Bet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerBettingSite
+{
+    public class Bet
+    {
+
+        public static void Main(String[] args)
+        {
+            String[] result = { "2-2", "2-0", "0-1", "3-3", "2-1" };
+            String[] bet = { "2-2", "3-1", "4-1", "0-1", "4-4" };
+            Console.WriteLine(getPrizeWon(result, bet));
+        }
+
+        public static String getPrizeWon(String[] result, String[] bet)
+        {
+            if (bet.Length < result.Length)
+            {
+                return "You lost the bet";
+            }
+
+            String[] betSplit = new String[2];
+            String[] resultSplit = new String[2];
+            int countValidScores = 0;
+            int countValidMatchWinners = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                betSplit = bet[i].Split("-");
+                resultSplit = result[i].Split("-");
+                if ((betSplit[0] == resultSplit[0]) && (betSplit[1] == resultSplit[1]))
+                {
+                    countValidScores++;
+                }
+
+                int res1 = Int32.Parse(betSplit[0]) - Int32.Parse(betSplit[1]);
+                int res2 = Int32.Parse(resultSplit[0]) - Int32.Parse(resultSplit[1]);
+                if ((res1 > 0 && res2 > 0) || (res1 == 0 && res2 == 0) || (res1 < 0 && res2 < 0))
+                {
+                    countValidMatchWinners++;
+                }
+            }
+
+            if (countValidScores == result.Length)
+            {
+                return "You won the Jackpot";
+            }
+
+            if (countValidMatchWinners == result.Length)
+            {
+                return "You won a Consolation Prize";
+            }
+
+            return "You lost the bet";
+        }
+    }
+}
